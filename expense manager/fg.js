1df4c570cb286195bd89b77d2bcf5f33bff19cd5
manaswinidas <dasmanaswini10@gmail.com>
2017-08-24 22:19:47 +0530

    Expense warning

--- expense manager/fg.js
+++ expense manager/fg.js
@@ -7,15 +7,17 @@ $(document).ready(function(){
             var limit = $("#limit").val();
             var amount = $("#amount").val();
             var percentage = amount/limit * 100;
-            console.log(percentage);
-            if (percentage >= 80) {
-    			error = "Warning! your expense is more than 80% of the limit";}
-    		$(".report").addClass("alert alert-warning").append(error);
+            if ($(percentage >= 80)) {
+      			  error = "Warning! Your expense is more than 80% of the limit";
+              $(".report").addClass("alert alert-warning").append(error);
+            }
             var method = $("#method").val();
             var markup = "<tr><td>" + name + "</td><td>" + category + "</td><td>" + limit + "</td><td>" + amount +"</td><td>" + method; + "</td></tr>"
             $(".table").append(markup)
-            if (amount >= limit) {
-             $(".new").addClass("alert alert-warning")	
-            }
+            if (amount > limit) {
+              $(".table tr:last").animate({
+                backgroundColor: "red"
+                });
+              }
         });
  });
