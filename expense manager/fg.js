b73d931cb701d3f56bdf0928b77f9d010dacf501
Anisha Swain <anisha.swain@yahoo.in>
2017-08-24 13:27:17 +0530

    expense manager3

--- expense manager/fg.js
+++ expense manager/fg.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
+		var myDiv =  $('.report');
+
         $("#savebutton").click(function(){
             var name = $("#name").val();
             var category = $("#category").val();
             var limit = $("#limit").val();
             var amount = $("#amount").val();
+            var percentage = amount/limit * 100;
+            console.log(percentage);
+            if (percentage >= 80) {
+    			error = "Warning! your expense is more than 80% of the limit";}
+    		$(".report").addClass("alert alert-warning").append(error);
             var method = $("#method").val();
             var markup = "<tr><td>" + name + "</td><td>" + category + "</td><td>" + limit + "</td><td>" + amount +"</td><td>" + method; + "</td></tr>"
-            $(".table").append(markup);
+            $(".table").append(markup)
+            if (amount >= limit) {
+             $(".new").addClass("alert alert-warning")	
+            }
         });
  });
